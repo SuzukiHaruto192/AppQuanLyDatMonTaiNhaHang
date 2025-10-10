6be980ec868d5e1e39e9c0ba81daaed5fe26ed6f
SuzukiHaruto192 <letamnatsu@gmail.com>
2025-10-11 00:28:30 +0700

    Thêm lại QuanLyMenu vào repo chính

---
+++ QuanLyMenu/MonAn.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+  public class MonAn : INotifyPropertyChanged
+    {
+        private string ten;
+        private int gia;
+        private string hinhAnh; // đường dẫn hình ảnh
+
+        public string Ten
+        {
+            get => ten;
+            set
+            {
+                if (ten != value)
+                {
+                    ten = value;
+                    OnPropertyChanged(nameof(Ten));
+                }
+            }
+        }
+
+        public int Gia
+        {
+            get => gia;
+            set
+            {
+                if (gia != value)
+                {
+                    gia = value;
+                    OnPropertyChanged(nameof(Gia));
+                }
+            }
+        }
+
+        public string HinhAnh
+        {
+            get => hinhAnh;
+            set
+            {
+                if (hinhAnh != value)
+                {
+                    hinhAnh = value;
+                    OnPropertyChanged(nameof(HinhAnh));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
