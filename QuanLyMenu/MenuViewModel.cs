563adb47bdffb288032592e27a2bc6e26cbf50b5
SuzukiHaruto192 <letamnatsu@gmail.com>
2025-10-11 00:43:32 +0700

    Thêm lại toàn bộ code QuanLyMenu vào repo chính

---
+++ QuanLyMenu/MenuViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class MenuViewModel
+    {
+    public ObservableCollection<MonAn> DanhSachMon { get; set; }
+
+    public MenuViewModel()
+    {
+        DanhSachMon = new ObservableCollection<MonAn>()
+        {
+            new MonAn(){ Ten="Phở bò", Gia=35000, HinhAnh="Images/pho.jpg" },
+            new MonAn(){ Ten="Bún chả", Gia=40000, HinhAnh="Images/buncha.jpg" }
+        };
+    }
+}
+
